527053b011541e3f314e90f6e50549a4f43ae09b
agentleman <675740051@qq.com>
2018-06-16 20:38:20 +0800

    webpack

--- webpack sample project/app/Greeter.js
+++ webpack sample project/app/Greeter.js
@@ -1,6 +1,26 @@
-//Greeter.js
-module.exports = function (){
-    var great = document.createElement('div');
-    great.textContent = "Hi there and greetings";
-    return great;
-}
+// //Greeter.js
+// var config = require('./config.json');
+//
+//
+// module.exports = function (){
+//     var great = document.createElement('div');
+//     great.textContent = "Hi there and greetings";
+//     return great;
+// }
+
+
+import React, {Component} from 'react';
+import config from './config.json';
+
+class Greeter extends Component{
+    render() {
+        return (
+            <div>
+            {config.greetText}
+    </div>
+    );
+    }
+};
+
+export default Greeter;
+
